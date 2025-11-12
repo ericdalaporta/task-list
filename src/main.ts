d0a087532595ef8d1636e255a0f5cd65f4d22376
Eric Dala Porta <ericdasilvadalaporta@gmail.com>
2025-11-12 20:36:47 -0300

    Delete src directory

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { ComponenteRaiz } from './app/app.component';
-
-// Inicializa a aplicação com o componente raiz
-bootstrapApplication(ComponenteRaiz, appConfig)
-  .catch((err) => console.error(err));
